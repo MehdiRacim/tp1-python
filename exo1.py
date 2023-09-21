f93c1ee59cb4f900fe08d0c4cf9c98ce806eef53
MehdiRacim <mehdibousetta03@gmail.com>
2023-09-21 16:12:29 +0200

    nikmok

---
+++ exo1.py
@@ -0,0 +1,28 @@
+b = 0
+c = 0
+d = 0
+print("choisir une operation a faire entre :addition, soustraction, multiplication ou division.")
+a = input()
+if a == "addition" :
+    b = int(input("donner un nombre : "))
+    c = int(input("donner un nombre : "))
+    d = b + c
+    print(f"la soustraction de {b} + {c} : {d}")
+
+if a == "soustraction" :
+    b = int(input("donner un nombre : "))
+    c = int(input("donner un nombre : "))
+    d = b - c
+    print(f"la soustraction de {b} - {c} : {d}")
+
+if a == "multiplication" :
+     b = int(input("donner un nombre : "))
+     c = int(input("donner un nombre : "))
+     d = b*c
+     print(f"la soustraction de {b} * {c} : {d}")
+    
+if a == "division" :
+    b = int(input("donner un nombre : "))
+    c = int(input(" donner un nombre : "))
+    print(f"la soustraction de {b} / {c} : {d}")
+
---
+++ exo2.py
@@ -0,0 +1,12 @@
+n = int(input("donner un nombre entier positif : "))
+i = 1
+b = 1
+a = 0
+while i <= n :
+    b = b * i 
+    a = a + i
+    i = i + 1
+
+print(b,a)
+
+    
