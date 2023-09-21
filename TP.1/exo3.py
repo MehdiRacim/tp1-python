ee1cc3f90cbf534d66a5e2d5a4a6cd3c805363de
Nemo <nemo.muller@ynov.com>
2023-09-21 17:22:53 +0200

    Update exo3

--- TP.1/exo3.py
+++ TP.1/exo3.py
@@ -1,6 +1,8 @@
+
+a = input("choisir un verbe :")
 from verbecc import Conjugator
 cg = Conjugator(lang='fr')
-conjugation = cg.conjugate('manger')
+conjugation = cg.conjugate(a)
 présent = conjugation['moods']['indicatif']['présent']
 for élément in présent : 
     print(élément)
