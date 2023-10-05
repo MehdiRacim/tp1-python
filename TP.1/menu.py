f5e97fc87a239de38019c5aef288f4055ee28ebf
Nemo <nemo.muller@ynov.com>
2023-10-05 15:55:43 +0200

    push

--- TP.1/menu.py
+++ TP.1/menu.py
@@ -1,5 +1,5 @@
 def restart():
-    print("Choisir un exercise du TP1 à lancer : \n1- exo1\n2- exo2\n3- exo3\n4- exo4\n5- exo5")
+    print("Choisir un exercise du TP1 à lancer : \n1- exo1\n2- exo2\n3- exo3\n4- exo4\n5- exo5\n6- Quitter")
     answer = input()
     if answer== "1" :
         with open("exo1.py") as f:
@@ -14,8 +14,11 @@ def restart():
     elif answer== "5" :
         with open("exo5.py") as f:
             exec(f.read())
+    elif answer== "6" : 
+        exit()
     else:
         print('Exercise invalide ou réponse inattendu... veuillez recommencer !')
         restart()
+
         
 restart()
